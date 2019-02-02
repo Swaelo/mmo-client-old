be60e2c06571b370d7b6fa1509959ced6ae72145
Swaelo <harleylaurie@rocketmail.com>
2019-02-03 02:35:58 +1000

    mouse is now locked to the screen, escape releases the mouse, clicking back on the game view will lock it again

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -17,15 +17,40 @@ public class PlayerCamera : MonoBehaviour
     private float CurrentX = 0f;
     private float CurrentY = 0f;
 
+    private bool MouseLocked = true;
+
     private void Start()
     {
         Vector3 angles = transform.eulerAngles;
         CurrentX = angles.x;
         CurrentY = angles.y;
+        //Lock the mouse cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        //Pressing escape while the cursor is locked releases it
+        if(Input.GetKeyDown(KeyCode.Escape) && MouseLocked)
+        {
+            MouseLocked = false;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        
+        //Clicking on the game window while the cursor is free locks it again
+        if(Input.GetMouseButton(0) && !MouseLocked)
+        {
+            MouseLocked = true;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     private void LateUpdate()
     {
+        //Cant control the camera when the mouse cursor has been released
+        if (!MouseLocked)
+            return;
+
         CurrentX += Input.GetAxis("Mouse X") * XSpeed * CameraDistance * 0.02f;
         CurrentY -= Input.GetAxis("Mouse Y") * YSpeed * 0.02f;
         CurrentY = ClampAngle(CurrentY, YMinimum, YMaximum);
